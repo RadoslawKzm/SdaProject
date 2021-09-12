a80b5f0121e91cc3b278ca6f40f33e6d2e016b48
rdkkz <Bolszewo11>
2021-09-12 11:59:32 +0200

    finished filter json example

--- App/code_folder/filter_json.py
+++ App/code_folder/filter_json.py
@@ -1,18 +1,18 @@
 import pickle
+from typing import List, Dict
+
 
 with open("json.pickle", "rb") as file:
     our_json = pickle.load(file)
 
 
-def filter_json(_json: list[dict], id_stacji: int) -> dict[int:dict]:
+def filter_json(_json: List[Dict], id_stacji: str) -> Dict:
     """Given list in _json variable return a dict that contains {id_stacji: {whole dict for given station}}"""
-    print(f"{id_stacji = }")
-    print(_json)
-    dict_1 = {}
-    for item in _json:
-        dict_1[item["id_stacji"]] = item
+    # dict_1 = {}
+    # for item in _json:
+    #     dict_1[item["id_stacji"]] = item
+    # dict_1 = dict_1[id_stacji]
+    return {item["id_stacji"]: item for item in _json}[id_stacji]
 
-    return {dict_["id_stacji"]: dict_ for dict_ in _json}
 
 output = filter_json(_json=our_json, id_stacji=12650)
-print("pass")
