49f9300347d9d40ae3998a34449c23a63263b9a2
rdkkz <Bolszewo11>
2021-09-12 13:36:07 +0200

    finished tests example with positive test case

--- App/tests/test_filter_json/test_filter_json.py
+++ App/tests/test_filter_json/test_filter_json.py
@@ -1,4 +1,8 @@
-import pytest
+from App.code_folder.filter_json import filter_json
+from json_input import json
+from json_expected_output import expected_output
+
 
 def test_positive_case():
-    assert 1 == 1
+    output = filter_json(json, "12600")
+    assert output == expected_output
