0c4cceb2647699892667f19e9a645988fd2f50fd
rdkkz <Bolszewo11>
2021-09-12 11:44:27 +0200

    updated filter json example

--- App/code_folder/filter_json.py
+++ App/code_folder/filter_json.py
@@ -8,6 +8,11 @@ def filter_json(_json: list[dict], id_stacji: int) -> dict[int:dict]:
     """Given list in _json variable return a dict that contains {id_stacji: {whole dict for given station}}"""
     print(f"{id_stacji = }")
     print(_json)
+    dict_1 = {}
+    for item in _json:
+        dict_1[item["id_stacji"]] = item
 
+    return {dict_["id_stacji"]: dict_ for dict_ in _json}
 
-filter_json(_json=our_json, id_stacji=12650)
+output = filter_json(_json=our_json, id_stacji=12650)
+print("pass")
