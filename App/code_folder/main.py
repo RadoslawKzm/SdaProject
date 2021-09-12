4f0bb30be8b35e81f55883d84026bb8408fdea64
rdkkz <Bolszewo11>
2021-09-12 09:10:05 +0200

    initial commit

---
+++ App/code_folder/main.py
@@ -0,0 +1,26 @@
+# pip3 install imports
+from fastapi import FastAPI, HTTPException, status, Request
+from fastapi.responses import JSONResponse
+
+app = FastAPI()
+
+
+@app.get("/get")
+async def get_data():
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"key": "some value"})
+
+
+@app.get("/get_params")
+async def get_with_params(skip: int = 0, limit: int = 10):
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"key": "some value"})
+
+
+@app.post("/post")
+async def post_data(request: Request):
+    return JSONResponse(status_code=status.HTTP_200_OK, content={"key": "some value"})
+
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
