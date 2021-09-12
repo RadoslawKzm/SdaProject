8897499a1b940bca61b954598ab570991d1e31d1
rdkkz <Bolszewo11>
2021-09-12 11:12:17 +0200

    added filter json template

--- App/code_folder/main.py
+++ App/code_folder/main.py
@@ -6,12 +6,16 @@ import requests
 app = FastAPI()
 
 
+def filter_json(_json: list[dict], id_stacji: int) -> dict[int:dict]:
+    pass
+
+
 @app.get("/get")
 async def get_data():
     response = requests.get("https://danepubliczne.imgw.pl/api/data/synop")
     json = response.json()
     station = json[10]
-    dictio = {}
+    dictio = {}  # {12650:{'id_stacji': {'12650', 'stacja': 'Kasprowy Wierch', 'data_pomiaru': '2021-09-12', 'godzina_pomiaru': '8', 'temperatura': '9.2', 'predkosc_wiatru': '2', 'kierunek_wiatru': '60', 'wilgotnosc_wzgledna': '67.6', 'suma_opadu': '0', 'cisnienie': None}}
     return JSONResponse(status_code=status.HTTP_200_OK, content=station)
 
 
