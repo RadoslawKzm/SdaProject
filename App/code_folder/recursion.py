8c0b16f1bf19e6035dc5027840760d12d17600f7
rdkkz <Bolszewo11>
2021-09-18 13:37:53 +0200

    recursion updated

--- App/code_folder/recursion.py
+++ App/code_folder/recursion.py
@@ -4,6 +4,9 @@ class Node:
         self.value = value
         self.nodes = []
 
+    def __repr__(self) -> str:
+        return f"{self.name}"
+
 
 if __name__ == '__main__':
     n1 = Node(name="n1", value=1)
@@ -11,3 +14,5 @@ if __name__ == '__main__':
     n3 = Node(name="n3", value=3)
     n4 = Node(name="n4", value=4)
     n5 = Node(name="n5", value=5)
+    n1.nodes.extend([n2, n3])
+    n2.nodes.extend([n4, n5])
