c97e14c4ffb9a500ea827fd64ed2a231005d3ef8
rdkkz <Bolszewo11>
2021-09-18 09:39:56 +0200

    added oop explanation

---
+++ App/code_folder/OOP_explanation.py
@@ -0,0 +1,38 @@
+class Planet:
+    counter = 0
+
+    def __init__(self, *, name: str):
+        self.name = name
+        self.update_counter()
+
+    def make_some_noise(self):
+        print(f"{self.name} is making some noise")
+
+    @classmethod
+    def update_counter(cls):
+        cls.counter += 1
+
+
+if __name__ == "__main__":
+    mars = Planet(name="mars")
+    jowisz = Planet(name="jowisz")
+    saturn = Planet(name="saturn")
+    neptun = Planet(name="neptun")
+    uran = Planet(name="uran")
+    lst = [mars, jowisz, saturn, uran, neptun]
+    dictio = {"mars": mars, "jowisz": jowisz, "saturn": saturn, "neptun": neptun, "uran": uran}
+
+    # lst = [
+    #     Planet(name="mars"),
+    #     Planet(name="jowisz"),
+    #     Planet(name="saturn"),
+    #     Planet(name="neptun"),
+    #     Planet(name="uran"),
+    # ]
+    # dictio = {
+    #     "mars": Planet(name="mars"),
+    #     "jowisz": Planet(name="jowisz"),
+    #     "saturn": Planet(name="saturn"),
+    #     "neptun": Planet(name="neptun"),
+    #     "uran": Planet(name="uran")
+    # }
