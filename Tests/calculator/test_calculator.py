cd4495d86afdc56d6dc8027b240699371384e2e2
rdkkz <Bolszewo11>
2021-09-25 13:05:43 +0200

    added mock example

--- Tests/calculator/test_calculator.py
+++ Tests/calculator/test_calculator.py
@@ -5,8 +5,8 @@ from calculator import Calculator
 
 
 class Test:
-    # def setup_method(self):
-    #     self.calc = Calculator()
+    def setup_method(self):
+        self.calc = Calculator()
 
     def test_mock(self):
         fake_calc = MagicMock()
@@ -18,68 +18,68 @@ class Test:
         retval = fake_calc.add(test_val_1, test_val_2)
         assert expected_result == retval
 
-    # @pytest.mark.xfail
-    # def test_expected_fail(self):
-    #     retval = self.calc.add(5, 2)
-    #     assert retval == 5
-    #
-    # @pytest.mark.dependency()
-    # def test_addition(self):
-    #     for _ in range(1000):
-    #         test_val_1 = random.uniform(-1000, 1000)
-    #         test_val_2 = random.uniform(-1000, 1000)
-    #         expected_result = test_val_1 + test_val_2
-    #
-    #         retval = self.calc.add(test_val_1, test_val_2)
-    #         assert expected_result != retval
-    #
-    # @pytest.mark.dependency(depends=["test_addition"])
-    # def test_addition1(self):
-    #     test_val_1 = 5.5
-    #     test_val_2 = 6.5
-    #     expected_result = test_val_1 + test_val_2
-    #
-    #     retval = self.calc.add(test_val_1, test_val_2)
-    #     assert expected_result == retval
-    #
-    # def test_addition2(self):
-    #     test_val_1 = "dupa"
-    #     test_val_2 = "dupa2"
-    #     expected_result = test_val_1 + test_val_2
-    #
-    #     with pytest.raises(ValueError):
-    #         retval = self.calc.add(test_val_1, test_val_2)
-    #         assert expected_result == retval
-    #
-    # def test_addition3(self):
-    #     test_val_1 = (1, 2, 3)
-    #     test_val_2 = (4, 5, 6)
-    #     expected_result = test_val_1 + test_val_2
-    #
-    #     with pytest.raises(ValueError):
-    #         retval = self.calc.add(test_val_1, test_val_2)
-    #         assert expected_result == retval
-    #
-    # def test_addition4(self):
-    #     test_val_1 = [1, 2, 3]
-    #     test_val_2 = [4, 5, 6]
-    #     expected_result = test_val_1 + test_val_2
-    #
-    #     with pytest.raises(ValueError):
-    #         retval = self.calc.add(test_val_1, test_val_2)
-    #         assert expected_result == retval
-    #
-    # def test_addition5(self):
-    #     test_val_1 = {1: 1, 2: 2, 3: 3}
-    #     test_val_2 = {4: 4, 5: 5, 6: 6}
-    #     expected_result = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6}
-    #
-    #     with pytest.raises(ValueError):
-    #         retval = self.calc.add(test_val_1, test_val_2)
-    #         assert expected_result == retval
-    #
-    # def teardown_method(self):
-    #     del self.calc
+    @pytest.mark.xfail
+    def test_expected_fail(self):
+        retval = self.calc.add(5, 2)
+        assert retval == 5
+
+    @pytest.mark.dependency()
+    def test_addition(self):
+        for _ in range(1000):
+            test_val_1 = random.uniform(-1000, 1000)
+            test_val_2 = random.uniform(-1000, 1000)
+            expected_result = test_val_1 + test_val_2
+
+            retval = self.calc.add(test_val_1, test_val_2)
+            assert expected_result != retval
+
+    @pytest.mark.dependency(depends=["test_addition"])
+    def test_addition1(self):
+        test_val_1 = 5.5
+        test_val_2 = 6.5
+        expected_result = test_val_1 + test_val_2
+
+        retval = self.calc.add(test_val_1, test_val_2)
+        assert expected_result == retval
+
+    def test_addition2(self):
+        test_val_1 = "dupa"
+        test_val_2 = "dupa2"
+        expected_result = test_val_1 + test_val_2
+
+        with pytest.raises(ValueError):
+            retval = self.calc.add(test_val_1, test_val_2)
+            assert expected_result == retval
+
+    def test_addition3(self):
+        test_val_1 = (1, 2, 3)
+        test_val_2 = (4, 5, 6)
+        expected_result = test_val_1 + test_val_2
+
+        with pytest.raises(ValueError):
+            retval = self.calc.add(test_val_1, test_val_2)
+            assert expected_result == retval
+
+    def test_addition4(self):
+        test_val_1 = [1, 2, 3]
+        test_val_2 = [4, 5, 6]
+        expected_result = test_val_1 + test_val_2
+
+        with pytest.raises(ValueError):
+            retval = self.calc.add(test_val_1, test_val_2)
+            assert expected_result == retval
+
+    def test_addition5(self):
+        test_val_1 = {1: 1, 2: 2, 3: 3}
+        test_val_2 = {4: 4, 5: 5, 6: 6}
+        expected_result = {1: 1, 2: 2, 3: 3, 4: 4, 5: 5, 6: 6}
+
+        with pytest.raises(ValueError):
+            retval = self.calc.add(test_val_1, test_val_2)
+            assert expected_result == retval
+
+    def teardown_method(self):
+        del self.calc
 
 
 if __name__ == "__main__":
