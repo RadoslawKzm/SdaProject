80c0dbbadb8908aae6aac057a8edd161430d6286
rdkkz <Bolszewo11>
2021-09-25 10:48:30 +0200

    failed tests

--- Tests/calculator/calculator.py
+++ Tests/calculator/calculator.py
@@ -1,7 +1,7 @@
 """
 Should support:
 1. Addition
- - only floats
+ - only floats input sanitization
 2. Subtraction
 3. Division
 4. Multiplication
@@ -13,4 +13,8 @@ Should support:
 class Calculator:
     @staticmethod
     def add(x: float, y: float) -> float:
+        if not isinstance(x, float):
+            raise ValueError(f"Float is expected. Input {x = } is type of {type(x)}.")
+        if not isinstance(y, float):
+            raise ValueError(f"Float is expected. Input {y = } is type of {type(y)}.")
         return x + y
