2299f4c73549c2cf0839a756f8c4e46a234c6ce2
rdkkz <Bolszewo11>
2021-09-25 11:07:18 +0200

    updated add tests

--- Tests/calculator/calculator.py
+++ Tests/calculator/calculator.py
@@ -8,13 +8,18 @@ Should support:
 5. Powering
 6. Factorial
 """
+from typing import Union
 
 
 class Calculator:
-    @staticmethod
-    def add(x: float, y: float) -> float:
-        if not isinstance(x, float):
-            raise ValueError(f"Float is expected. Input {x = } is type of {type(x)}.")
-        if not isinstance(y, float):
-            raise ValueError(f"Float is expected. Input {y = } is type of {type(y)}.")
+    SUPPORTED_TYPES: tuple = (int, float)
+
+    def check_input(self, in_1: Union[int, float], in_2: Union[int, float]):
+        if not isinstance(in_1, self.SUPPORTED_TYPES):
+            raise ValueError(f"Float is expected. Input {in_1 = } is type of {type(in_1)}.")
+        if not isinstance(in_2, self.SUPPORTED_TYPES):
+            raise ValueError(f"Float is expected. Input {in_2 = } is type of {type(in_2)}.")
+
+    def add(self, x: float, y: float) -> float:
+        self.check_input(x, y)
         return x + y
--- Tests/calculator/test_calculator.py
+++ Tests/calculator/test_calculator.py
@@ -12,44 +12,58 @@ def test_addition():
     assert expected_result == retval
 
 
+def test_addition1():
+    test_val_1 = 5.5
+    test_val_2 = 6.5
+    expected_result = test_val_1 + test_val_2
+
+    calc = Calculator()
+    retval = calc.add(test_val_1, test_val_2)
+    assert expected_result == retval
+
+
 def test_addition2():
     test_val_1 = "dupa"
     test_val_2 = "dupa2"
     expected_result = test_val_1 + test_val_2
 
     calc = Calculator()
-    retval = calc.add(test_val_1, test_val_2)
-    assert expected_result == retval
+    with pytest.raises(ValueError):
+        retval = calc.add(test_val_1, test_val_2)
+        assert expected_result == retval
 
 
 def test_addition3():
-    test_val_1 = (1,2,3)
-    test_val_2 = (4,5,6)
+    test_val_1 = (1, 2, 3)
+    test_val_2 = (4, 5, 6)
     expected_result = test_val_1 + test_val_2
 
     calc = Calculator()
-    retval = calc.add(test_val_1, test_val_2)
-    assert expected_result == retval
+    with pytest.raises(ValueError):
+        retval = calc.add(test_val_1, test_val_2)
+        assert expected_result == retval
 
 
 def test_addition4():
-    test_val_1 = [1,2,3]
-    test_val_2 = [4,5,6]
+    test_val_1 = [1, 2, 3]
+    test_val_2 = [4, 5, 6]
     expected_result = test_val_1 + test_val_2
 
     calc = Calculator()
-    retval = calc.add(test_val_1, test_val_2)
-    assert expected_result == retval
+    with pytest.raises(ValueError):
+        retval = calc.add(test_val_1, test_val_2)
+        assert expected_result == retval
 
 
 def test_addition5():
-    test_val_1 = {1:1, 2:2, 3:3}
-    test_val_2 = {4:4, 5:5, 6:6}
-    expected_result = test_val_1 + test_val_2
+    test_val_1 = {1: 1, 2: 2, 3: 3}
+    test_val_2 = {4: 4, 5: 5, 6: 6}
+    expected_result = {1: 1, 2: 2, 3: 3,4: 4, 5: 5, 6: 6}
 
     calc = Calculator()
-    retval = calc.add(test_val_1, test_val_2)
-    assert expected_result == retval
+    with pytest.raises(ValueError):
+        retval = calc.add(test_val_1, test_val_2)
+        assert expected_result == retval
 
 
 if __name__ == "__main__":
