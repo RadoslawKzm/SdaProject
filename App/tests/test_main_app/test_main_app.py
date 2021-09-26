18d945a20492bca8a0259b5fb75882fc257eac67
rdkkz <Bolszewo11>
2021-09-26 13:53:19 +0200

    added test imgw with station id

--- App/tests/test_main_app/test_main_app.py
+++ App/tests/test_main_app/test_main_app.py
@@ -7,12 +7,28 @@ from fastapi.testclient import TestClient
 # user created imports
 from App.code_folder.main import app
 from App.tests.test_main_app import input_data
-# from App.tests.test_main_app import output_data
 
+from App.tests.test_main_app import output_data
 
 client = TestClient(app)
 
-def test_api():
+
+def test_get_imgw_data():
+    response_object_mock = MagicMock()
+    response_object_mock.json.return_value = input_data.imgw_request_mock
+    response_object_mock.status_code = 200
+
+    request_mock = MagicMock()
+    request_mock.get.return_value = response_object_mock
+
+    with patch("App.code_folder.main.requests", request_mock):
+        response = client.get("/get")
+    assert response.status_code == 200
+    assert response.json() == input_data.imgw_request_mock
+
+
+def test_get_imgw_with_station_id():
+    STATION_ID = "12295"
     response_object_mock = MagicMock()
     response_object_mock.json.return_value = input_data.imgw_request_mock
     response_object_mock.status_code = 200
@@ -20,5 +36,7 @@ def test_api():
     request_mock = MagicMock()
     request_mock.get.return_value = response_object_mock
     with patch("App.code_folder.main.requests", request_mock):
-        response = client.get("/get")
-    assert response.json() == input_data.imgw_request_mock
+        response = client.get("/get_imgw_with_station_id", params={"station_id": STATION_ID})
+    assert response.status_code == 200
+    assert response.json() == {item["id_stacji"]: item for item in input_data.imgw_request_mock}[STATION_ID]
+
