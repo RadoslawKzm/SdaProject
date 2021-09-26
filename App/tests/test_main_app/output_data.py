c440acd8e4ce13c4f945968c5561734e586dfea3
rdkkz <Bolszewo11>
2021-09-26 14:28:45 +0200

    tests input/output data

---
+++ App/tests/test_main_app/output_data.py
@@ -0,0 +1,7 @@
+create_random_user_output_mock = [
+    {"name": "Aila", "last": "Skårdal", "age": 46, "email": "Aila.Skårdal@company.com"},
+    {"name": "Clara", "last": "Olsen", "age": 23, "email": "Clara.Olsen@company.com"},
+    {"name": "پارمیس", "last": "کامروا", "age": 37, "email": "پارمیس.کامروا@company.com"},
+    {"name": "Christiane", "last": "Torland", "age": 23, "email": "Christiane.Torland@company.com"},
+    {"name": "Emma", "last": "Peltonen", "age": 46, "email": "Emma.Peltonen@company.com"},
+]
