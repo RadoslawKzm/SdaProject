420d80c63201c80657a684673bd2f786cdeb5dcf
rdkkz <Bolszewo11>
2021-09-26 12:53:39 +0200

    updated first test case

--- App/tests/test_main_app/test_main_app.py
+++ App/tests/test_main_app/test_main_app.py
@@ -6,17 +6,19 @@ from fastapi.testclient import TestClient
 
 # user created imports
 from App.code_folder.main import app
-# from App.tests.test_main_app import input_data
+from App.tests.test_main_app import input_data
 # from App.tests.test_main_app import output_data
 
 
 client = TestClient(app)
 
-
-
 def test_api():
-    response = client.get("/get")
-    request_mock = MagicMock()
-    request_mock.get.return_value = 1
-    print("pass")
+    response_object_mock = MagicMock()
+    response_object_mock.json.return_value = input_data.imgw_request_mock
+    response_object_mock.status_code = 200
 
+    request_mock = MagicMock()
+    request_mock.get.return_value = response_object_mock
+    with patch("App.code_folder.main.requests", request_mock):
+        response = client.get("/get")
+    assert response.json() == input_data.imgw_request_mock
