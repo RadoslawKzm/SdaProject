9d29b4cbf07e35fc79e12d0d5d07832f80ec7bbe
rdkkz <Bolszewo11>
2021-09-26 10:10:32 +0200

    finished dockerizing test example

--- Tests/pizza_exercise_docker/entrypoint_tests.sh
+++ Tests/pizza_exercise_docker/entrypoint_tests.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 set -e
 
-cd /App/tests;
-coverage run --source /App/code_folder -m pytest --html=pytest_report.html --self-contained-html /App/tests  || true;
+source venv/bin/activate;
+cd tests;
+coverage run --source /Tests/pizza_exercise_docker/code_folder -m pytest --html=pytest_report.html --self-contained-html /Tests/pizza_exercise_docker/tests  || true;
 coverage report;
 coverage html;
-/bin/bash
 mkdir -p results
 rm -rf results/*
-mv /App/tests/htmlcov /App/tests/results/.
-mv /App/tests/pytest_report.html /App/tests/results/.
-mv /App/tests/.coverage /App/tests/results/.
-cd /App/tests/results/
+mv htmlcov results/.
+mv pytest_report.html results/.
+mv .coverage results/.
+cd results
 coverage-badge -o coverage.svg -f
