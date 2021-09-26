832a031d98996ba1a5f17deec47823615dbfc432
RadoslawKzm <47893557+RadoslawKzm@users.noreply.github.com>
2021-09-26 14:34:51 +0200

    Delete test_filter_json.py

--- App/tests/test_filter_json/test_filter_json.py
+++
@@ -1,9 +0,0 @@
-from json_expected_output import expected_output
-from json_input import json
-
-from App.code_folder.filter_json import filter_json
-
-
-def test_positive_case():
-    output = filter_json(json, "12600")
-    assert output == expected_output
