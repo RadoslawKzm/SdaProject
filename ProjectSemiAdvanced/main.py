0c946906f3afde352cdfe9a7e91d386f4a041786
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-10-09 13:27:10 +0200

    Semi advanced json

--- ProjectSemiAdvanced/main.py
+++ ProjectSemiAdvanced/main.py
@@ -1,3 +1,13 @@
+# pip3 install imports
+import json
 import numpy as np
 import pandas as pd
+import requests
 
+response = requests.get(url="https://randomuser.me/api/?results=50")
+output = response.json()
+with open("randomuser.json", "w") as file:
+    json.dump(output, file)
+
+
+print("pass")
