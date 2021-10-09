896aac68e972393606810238d93f3c8effacb0ef
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-10-09 15:34:03 +0200

    Semi advanced finished serialization task

--- ProjectSemiAdvanced/task_1.py
+++ ProjectSemiAdvanced/task_1.py
@@ -4,3 +4,13 @@
 3. create dataframe from unnested data
 4. save as new json and csv
 """
+import json
+import pandas as pd
+
+with open("randomuser.json") as file:
+    data = json.load(file)
+
+new_list2 = [{key: value for key, value in sub_dict.items() if type(value) != dict} for sub_dict in data]
+df = pd.DataFrame(new_list2)
+df.to_csv("test.csv")
+df.to_json("test.json")
