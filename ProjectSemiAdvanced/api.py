9ee27dcd8f06e7344dddd980cb69e375c709f63b
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-10-09 14:07:42 +0200

    Semi advanced adding api

---
+++ ProjectSemiAdvanced/api.py
@@ -0,0 +1,14 @@
+from fastapi import FastAPI, Request
+
+app = FastAPI()
+
+
+@app.post("/")
+def do_something(request=Request):
+    print("pass")
+
+
+if __name__ == '__main__':
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8000)
