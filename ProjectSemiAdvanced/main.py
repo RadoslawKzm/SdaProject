267e02967b479bbc7fbf711bb3e91c2f8c4fd078
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-10-09 14:05:19 +0200

    Semi advanced adding api

--- ProjectSemiAdvanced/main.py
+++ ProjectSemiAdvanced/main.py
@@ -1,7 +1,10 @@
 # pip3 install imports
 import json
+from typing import Dict
+
 import numpy as np
 import pandas as pd
+from pydantic import BaseModel
 import requests
 
 # response = requests.get(url="https://randomuser.me/api/?results=50")
@@ -14,4 +17,22 @@ with open("randomuser.json", "r") as file:
 
 df = pd.read_json("randomuser.json")
 
-print("pass")
+
+class RandomUser(BaseModel):
+    gender: str
+    name: dict
+    location: dict
+    email: str
+    login: dict
+    dob: dict
+    registered: dict
+    phone: str
+    cell: str
+    id: dict
+    picture: dict
+    nat: str
+
+
+tst: Dict = output[0]
+tst['gender'] = 1
+dupa = RandomUser(**output[0])
