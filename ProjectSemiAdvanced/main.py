f0225171bf8e72a4e9ca553ef0c23cb7db8c841c
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-10-09 13:40:07 +0200

    Semi advanced json

--- ProjectSemiAdvanced/main.py
+++ ProjectSemiAdvanced/main.py
@@ -4,10 +4,14 @@ import numpy as np
 import pandas as pd
 import requests
 
-response = requests.get(url="https://randomuser.me/api/?results=50")
-output = response.json()
-with open("randomuser.json", "w") as file:
-    json.dump(output, file)
+# response = requests.get(url="https://randomuser.me/api/?results=50")
+# output = response.json()
+# with open("randomuser.json", "w") as file:
+#     json.dump(output, file)
 
+with open("randomuser.json", "r") as file:
+    output = json.load(file)
+
+df = pd.read_json("randomuser.json")
 
 print("pass")
