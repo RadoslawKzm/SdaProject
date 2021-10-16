abcc98814cbebbbffde0a3e8f2fb2144cdf09788
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-10-16 11:26:59 +0200

    added regex example

---
+++ PythonSemiAdvanced/7-RegEx.py
@@ -0,0 +1,25 @@
+"""
+
+\d
+\w
++
+?
+
+"""
+
+import re
+
+# strin = "test 123_du1pa+test"
+# output_1 = re.sub(r"[ _+]", "", strin)
+# output_2 = re.findall(r"[dupa]", strin)
+# compiled_regex = re.compile(r"[dupa]")
+# output_3 = re.findall(compiled_regex, strin)
+# output_4 = re.match(r"d", strin)
+# output_5 = re.search(r"dupa", strin)
+
+st: set = set()
+
+with open("7-RegEx_example", "r") as file:
+    for line in file:
+        if text := re.findall(r"(?<=\* Cell {13}: )\w+", line):
+            st.add(text[0].strip())
