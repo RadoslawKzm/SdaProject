01a8829c3659c3f9cefef5f8ebda4cc326fb1681
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-06 13:52:54 +0100

    finished modeling

--- Recruitment_Cheat_Sheet/internship_task_1/solution/main.py
+++ Recruitment_Cheat_Sheet/internship_task_1/solution/main.py
@@ -4,7 +4,26 @@ from output import output_short
 
 
 class Movie:
-    """pass"""
+    movies = []
+
+    def __init__(self, dictio):
+        self.title = dictio["Title"]
+        self.year = dictio["Year"]
+        self.runtime = dictio["Runtime"]
+        self.genre = dictio["Genre"]
+        self.director = dictio["Director"]
+        self.actors = dictio["Actors"]
+        self.writer = dictio["Writer"]
+        self.language = dictio["Language"]
+        self.country = dictio["Country"]
+        self.awards = dictio["Awards"]
+        self.imdb_rating = dictio["imdbRating"]
+        self.imdb_votes = dictio["imdbVotes"]
+        self.box_office = dictio.get("BoxOffice", "")
+        self.update_movies()
+
+    def update_movies(self):
+        self.movies.append(self.__dict__)
 
 
 def get_movies_from_omdb():
@@ -26,7 +45,8 @@ def populate_csv():
     # pd.DataFrame(lst).to_csv("new_movies.csv", index_label="id")
     for movie_dict in output_short:
         movie = Movie(movie_dict)
-    pd.DataFrame(Movie.movie_list).to_csv("new_movies_2.csv")
+    pd.DataFrame(Movie.movies).to_csv("new_movies_2.csv")
+
 
 if __name__ == '__main__':
     # get_movies_from_omdb()
