86f121301350c4a7249e5fd94176d339ad4a0c20
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-07 10:14:58 +0100

    update

--- Algorithms/binary_search.py
+++ Algorithms/binary_search.py
@@ -3,5 +3,6 @@ from typing import List
 inpt: List[int] = list(range(512))
 
 
-def is_number_inside(num: int, collection: List[int]) -> bool:
+def is_number_inside(*, num: int, collection: List[int]) -> bool:
     """implement binary search"""
+
