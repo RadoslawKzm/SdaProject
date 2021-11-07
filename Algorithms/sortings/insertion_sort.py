d9507717597ff79fe1e85851357ca104ae3ed9ec
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-07 14:31:52 +0100

    added insertion sort task

---
+++ Algorithms/sortings/insertion_sort.py
@@ -0,0 +1,11 @@
+from typing import List
+
+
+def insertion_sort(arr: List[int]) -> None:
+    """Implement bubble sort"""
+
+
+if __name__ == '__main__':
+    arr = [8, 5, 2, 9, 5, 6, 3]
+    insertion_sort(arr)
+    print(arr)
