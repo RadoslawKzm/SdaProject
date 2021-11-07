ade07aeb1fba00d08c6da9b474b39eeb57182026
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-07 14:58:49 +0100

    finished insertion task

--- Algorithms/sortings/insertion_sort.py
+++ Algorithms/sortings/insertion_sort.py
@@ -2,7 +2,16 @@ from typing import List
 
 
 def insertion_sort(arr: List[int]) -> None:
-    """Implement bubble sort"""
+    """Implement insertion sort"""
+    start = 1
+    for index in range(1, len(arr)):
+        for sub_index in reversed(range(0, start)):
+            if arr[sub_index] > arr[index]:
+                arr[index], arr[sub_index] = arr[sub_index], arr[index]
+                index -=1
+                continue
+            break
+        start += 1
 
 
 if __name__ == '__main__':
