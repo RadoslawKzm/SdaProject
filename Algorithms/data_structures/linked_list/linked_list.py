8289d355a5a683259add9f08d658ed4fb9236dc2
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-20 11:46:30 +0100

    added reversing linked list

--- Algorithms/data_structures/linked_list/linked_list.py
+++ Algorithms/data_structures/linked_list/linked_list.py
@@ -11,11 +11,25 @@ class Node:
 
 class LinkedList:
     def __init__(self, sequence: Sequence[Any]):
-        self.node = None
+        self.head = None
         if sequence:
             for item in sequence:
-                self.node = Node(value=item, next_node=self.node)
+                self.head = Node(value=item, next_node=self.head)
+
+
+def reverse_node(*, head_node: Node, prev_node: Node = None) -> Node:
+    if not head_node.next_node:
+        head_node.next_node = prev_node
+        return head_node
+    last_node = reverse_node(head_node=head_node.next_node, prev_node=head_node)
+    head_node.next_node = prev_node
+    return last_node
+
+
+def reverse_linked_list(*, link_list: LinkedList) -> None:
+    link_list.head = reverse_node(head_node=link_list.head)
 
 
 if __name__ == '__main__':
     lst = LinkedList(sequence=[1, 2, 3, 4])
+    reverse_linked_list(link_list=lst)
