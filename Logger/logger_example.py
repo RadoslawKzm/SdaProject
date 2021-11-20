ee2fc3f4a45360c32d5f1e0fa7bbefd8c1649e7e
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-20 12:51:18 +0100

    added logger example

--- Logger/logger_example.py
+++ Logger/logger_example.py
@@ -16,3 +16,11 @@ def dummy4(*args, **kwargs):
 
 def dummy5(*args, **kwargs):
     print(f"Inside dummy5 function with {args} and {kwargs}")
+
+
+if __name__ == '__main__':
+    dummy1("Test1", 1, dupa=1, dupa2=1)
+    dummy2("Test2", 2, dupa=2, dupa2=2)
+    dummy3("Test3", 3, dupa=3, dupa2=3)
+    dummy4("Test4", 4, dupa=4, dupa2=4)
+    dummy5("Test5", 5, dupa=5, dupa2=5)
