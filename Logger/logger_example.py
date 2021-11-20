3f747896c61b3772f08a770aec4d846e7ec69386
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-20 12:50:03 +0100

    added logger example

---
+++ Logger/logger_example.py
@@ -0,0 +1,18 @@
+def dummy1(*args, **kwargs):
+    print(f"Inside dummy1 function with {args} and {kwargs}")
+
+
+def dummy2(*args, **kwargs):
+    print(f"Inside dummy2 function with {args} and {kwargs}")
+
+
+def dummy3(*args, **kwargs):
+    print(f"Inside dummy3 function with {args} and {kwargs}")
+
+
+def dummy4(*args, **kwargs):
+    print(f"Inside dummy4 function with {args} and {kwargs}")
+
+
+def dummy5(*args, **kwargs):
+    print(f"Inside dummy5 function with {args} and {kwargs}")
