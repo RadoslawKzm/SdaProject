ebabbdb142f82826423d6dfab94a35d8947e9da9
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-11-28 11:27:18 +0100

    factory exercise

--- DesignPatterns/factory/factory.py
+++ DesignPatterns/factory/factory.py
@@ -1,16 +1,12 @@
 from DesignPatterns.factory.plugins.character_abc import CharacterABC
-from plugins.bard import Bard
-from plugins.wizard import Wizard
-from plugins.tank import Tank
-
-tank = Tank()
-wizard = Wizard()
-bard = Bard()
 
 
-# characters = [tank, wizard, bard]
-# for character in characters:
-#     character.make_noise()
+def factory(path="plugins/characters.json"):
+    with open(path) as f:
+        print("pass")
+
+
+
 
 def func(characters: list[CharacterABC]):
     for character in characters:
@@ -19,4 +15,4 @@ def func(characters: list[CharacterABC]):
 
 
 if __name__ == '__main__':
-    func([tank, wizard, bard])
+   factory()
