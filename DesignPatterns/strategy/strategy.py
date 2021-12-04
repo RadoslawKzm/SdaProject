0acf10a3c07bf47573046de41f492cc8943a26b4
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-12-04 11:41:56 +0100

    proxy finished

--- DesignPatterns/strategy/strategy.py
+++ DesignPatterns/strategy/strategy.py
@@ -1,6 +1,15 @@
+from random import shuffle
+
+from Algorithms.sortings.insertion_sort import insertion_sort
+from Algorithms.sortings.selection_sort import selection_sort
+from Algorithms.sortings.buble_sort import bubble_sort
+
+
 def sort(*, sort_type: str, obj_to_sort: list[int]) -> list[int]:
     """Implement sorting algrithms: bubble, insertion, selection, random"""
-    pass
+    dictio = {"bubble": bubble_sort, "insertion": insertion_sort, "selection": selection_sort, "random": shuffle}
+    dictio[sort_type](obj_to_sort)
+    return obj_to_sort
 
 
 if __name__ == '__main__':
