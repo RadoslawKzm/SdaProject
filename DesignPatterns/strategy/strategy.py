ba01bc4d8c6a0059453e0866c0557882e0523088
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-12-04 13:40:54 +0100

    finished strategy pattern

--- DesignPatterns/strategy/strategy.py
+++ DesignPatterns/strategy/strategy.py
@@ -1,16 +1,12 @@
-from random import shuffle
-
-from Algorithms.sortings.insertion_sort import insertion_sort
-from Algorithms.sortings.selection_sort import selection_sort
-from Algorithms.sortings.buble_sort import bubble_sort
+from DesignPatterns.strategy.sorting_algo_interface import SortingStrategyABC
 
 
 def sort(*, sort_type: str, obj_to_sort: list[int]) -> list[int]:
     """Implement sorting algrithms: bubble, insertion, selection, random"""
-    dictio = {"bubble": bubble_sort, "insertion": insertion_sort, "selection": selection_sort, "random": shuffle}
-    dictio[sort_type](obj_to_sort)
+
+    SortingStrategyABC.subclasses_as_dict().get(sort_type).inplace_sort(obj_to_sort=obj_to_sort)
     return obj_to_sort
 
 
 if __name__ == '__main__':
-    sort(sort_type="random", obj_to_sort=[5, 2, 9, 6, 5, 1, 8, 0, 2, 4, 5])
+    dupa = sort(sort_type="bubble", obj_to_sort=[5, 2, 9, 6, 5, 1, 8, 0, 2, 4, 5])
