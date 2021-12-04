46951745f154a6ede699d4e99898d40a96475009
Radoslaw Kuzma <rdkkzm@gmail.com>
2021-12-04 10:39:54 +0100

    proxy finished

---
+++ DesignPatterns/strategy/strategy.py
@@ -0,0 +1,7 @@
+def sort(*, sort_type: str, obj_to_sort: list[int]) -> list[int]:
+    """Implement sorting algrithms: bubble, insertion, selection, random"""
+    pass
+
+
+if __name__ == '__main__':
+    sort(sort_type="random", obj_to_sort=[5, 2, 9, 6, 5, 1, 8, 0, 2, 4, 5])
